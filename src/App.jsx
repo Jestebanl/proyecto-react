3f07fe251cbc591bd51bad9c5c993cf6a5672af8
Juan <jestebanl2004@gmail.com>
2025-03-01 16:14:10 +0100

    Vaciado css innecesario y modificado App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+import producto1Depi from '/img/producto1Depi.png'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import Productos from './componentes/Productos/Productos'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Productos
+      img={producto1Depi}
+      titulo="Tarjeta Regalo"
+      texto="Si no sabes que regalar, una tarjeta regalo puede ser tu mejor decision para esa persona tan especial."
+      coste="29.99€"
+    />
   )
 }
 
