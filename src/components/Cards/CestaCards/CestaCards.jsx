4c5a597d4b83922c50ea9b3d255fc2d4fab7f742
Juan <jestebanl2004@gmail.com>
2025-03-10 13:58:10 +0100

    estilos Cesta

--- src/components/Cards/CestaCards/CestaCards.jsx
+++ src/components/Cards/CestaCards/CestaCards.jsx
@@ -2,27 +2,14 @@ import './CestaCards.css'
 
 function CestaCards(props) {
   return (
-    <div className="products">
-      <div className="product">
-        <img src={props.img} alt={props.titulo} className='img' />
-        <div>
-          <span>{props.titulo}</span>
-        </div>
-        {/* <div className="quantity">
-                <button>
-                  <svg fill="none" viewBox="0 0 24 24" height={14} width={14} xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" stroke="#47484b" d="M20 12L4 12" />
-                  </svg>
-                </button>
-                <label>2</label>
-                <button>
-                  <svg fill="none" viewBox="0 0 24 24" height={14} width={14} xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" stroke="#47484b" d="M12 4V20M20 12H4" />
-                  </svg>
-                </button>
-              </div> */}
-        <label className="price small">{props.coste}</label>
+
+    <div className="product">
+      <img src={props.img} alt={props.titulo} className='img' />
+      <div>
+        <span>{props.titulo}</span>
       </div>
+      <label className="price small">{props.coste}</label>
+
     </div>
   )
 }
--- src/components/Pages/Cesta/Cesta.jsx
+++ src/components/Pages/Cesta/Cesta.jsx
@@ -1,19 +1,20 @@
 import CestaCards from "../../Cards/CestaCards/CestaCards"
 import './Cesta.css'
 
-function Cesta({cesta}) {
+function Cesta({ cesta }) {
   return (
-    <div className="card cart">
-    <label className="title">Your cart</label>
-
-      {cesta.map((producto) => (
-        <CestaCards
-          key = {producto.id}
-          img={producto.img}
-          titulo={producto.titulo}
-          coste={producto.coste}
-        />
-      ))}
+    <div className="cesta-container">
+      <label className="title">Your cart</label>
+      <div className="products">
+        {cesta.map((producto) => (
+          <CestaCards
+            key={producto.id}
+            img={producto.img}
+            titulo={producto.titulo}
+            coste={producto.coste}
+          />
+        ))}
+      </div>
     </div>
   )
 }
